9cd2c01d4c10ae4b5ea61e3b7ea191cca4d7d3a8
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-11-07 12:20:30 +0530

    Update App component to display a message

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
+ 
+   return <p>I am learning React. My life is getting better.</p>;
+  
 }
 
 
